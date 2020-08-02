07a890cad949b3c36a84c8f55c1a05d02fa91a6d
NicolasBenoitQC <miminicoqc@gmail.com>
2020-08-02 18:21:12 -0400

    delete some unused file.

--- ecosystem_mindmap/src/App.tsx
+++ ecosystem_mindmap/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { MindMapBuilder } from './mindMapBuilder/MindMapBuilder';
-import { EditingCells } from './cells/EditingCells';
 
 const  App: React.FC = () => {
   return (
     <Router>
       <div className='container' >
         <Route path='/' exact component={MindMapBuilder} />
-        <Route path='/edit/:id_cell/:id_stemCell' component={EditingCells} />
       </div>
     </Router>
   );
--- ecosystem_mindmap/src/cells/ButtonAddCell.tsx
+++ ecosystem_mindmap/src/cells/ButtonAddCell.tsx
@@ -1,5 +1,5 @@
 // Framwork
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import io from 'socket.io-client';
 
 // Framwork material-ui
